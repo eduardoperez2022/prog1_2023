ac2d2e1d9722307d2eb58e2e19ef9bcc05df67ca
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2023-08-29 19:32:47 -0300

    Update Demo_excepciones.java

--- demo_excepciones/Demo_excepciones.java
+++ demo_excepciones/Demo_excepciones.java
@@ -1,27 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package demo_excepciones;
 
-/**
- *
- * @author eduardo
- */
 public class Demo_excepciones {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
-        ejemplo2 eje2 = new ejemplo2();
+        // ejemplo2 eje2 = new ejemplo2();
+        ejemplo2_solucion eje2 = new ejemplo2_solucion();
         
-        // eje2.convierte_a_numero();
+        eje2.convierte_a_numero();
         
-        // eje2.divide_por_cero();
+        eje2.divide_por_cero();
 
-        // eje2.elemento_array();
+        eje2.elemento_array();
         
         eje2.divide_por_cero_tipo();
 
