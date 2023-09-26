540370ab5aea7db43fda80a8205ff60f003c4388
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2023-09-25 23:29:25 -0300

    Varios casos de algoritmos de recursividad

---
+++ demo_recursividad_2/Demo_recursividad_2.java
@@ -0,0 +1,12 @@
+package demo_recursividad_2;
+
+public class Demo_recursividad_2 {
+
+    public static void main(String[] args) {
+        
+        menu men = new menu();
+        men.iniciar();
+
+    }
+    
+}
